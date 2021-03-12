08e34c4292fd1b51af27356fdbce2b032bccc389
Bastian Oppermann <bastianoppermann1997@gmail.com>
2021-03-12 18:35:31 +0100

    Remove all template dummy content

---
+++ src/routes/services/[id([0-9]+)].svelte
@@ -0,0 +1,12 @@
+<script context="module" lang="ts">
+    export async function preload(page, session) {
+        const { id } = page.params;
+        return { id };
+    }
+</script>
+
+<script lang="ts">
+    export let id: string;
+</script>
+
+<h1>Plugin id: {id}</h1>
