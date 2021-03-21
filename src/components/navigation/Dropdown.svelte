cc84fefe153b0bfa77e90ca0c2949a9b6449fab8
Bastian Oppermann <bastianoppermann1997@gmail.com>
2021-03-21 15:16:54 +0100

    Slightly decrease dropdown padding

--- src/components/navigation/Dropdown.svelte
+++ src/components/navigation/Dropdown.svelte
@@ -11,13 +11,13 @@
     <div transition:fade={{ duration: 100 }} class="absolute z-50 mt-2 ml-4">
         <div class="overflow-hidden text-black bg-white rounded-lg ring-1 ring-black ring-opacity-5 shadow-lg dark:ring-opacity-10 dark:ring-white dark:bg-gray-800 dark:text-gray-200">
             {#each links as link}
-                <a href={link.href} class="block p-5 w-full hover:bg-gray-200 dark:hover:bg-gray-700">
+                <a href={link.href} class="block p-4 w-full hover:bg-gray-200 dark:hover:bg-gray-700">
                     {link.text}
                     <slot name="link-appendix" {link}/>
                 </a>
             {/each}
             {#if actionLabel}
-                <a href="/" class="block inline-flex justify-center items-center p-4 w-full text-gray-900 bg-gray-100 hover:bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-700">
+                <a href="/" class="block inline-flex justify-center items-center p-3 w-full text-gray-900 bg-gray-100 hover:bg-gray-200 dark:bg-gray-900 dark:hover:bg-gray-700">
                     {#if $$slots['action-icon']}
                         <span class="w-6 h-6 text-blue-500 dark:text-blue-400">
                             <slot name="action-icon"/>
