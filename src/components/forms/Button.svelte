5a780c180cfbbfb227bb6610824c959fc6a2792e
Bastian Oppermann <bastianoppermann1997@gmail.com>
2021-05-13 13:07:46 +0200

    Add "disabled" property for Button component

--- src/components/forms/Button.svelte
+++ src/components/forms/Button.svelte
@@ -3,6 +3,7 @@
     export { className as class };
 
     export let small = false;
+    export let disabled = false;
     export let type: string = undefined;
 </script>
 
@@ -10,7 +11,16 @@
     on:click
     {type}
     class:text-sm={small}
+    class:opacity-60={disabled}
+    {disabled}
     class="bg-blue-800 text-white py-2 px-6 rounded focus:outline-none focus:ring focus:border-blue-300 {className}"
 >
     <slot />
 </button>
+
+<style>
+    button:disabled {
+        cursor: default;
+        pointer-events: all;
+    }
+</style>
