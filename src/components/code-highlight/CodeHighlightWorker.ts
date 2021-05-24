a8fa66f43b6e23461ebd960684e32fe3348987c6
Bastian Oppermann <bastianoppermann1997@gmail.com>
2021-05-24 22:03:37 +0200

    Reduce bundle size on "Include Metrics" page
    Only import the used languages from highlight.js

--- src/components/code-highlight/CodeHighlightWorker.ts
+++ src/components/code-highlight/CodeHighlightWorker.ts
@@ -1,4 +1,11 @@
-import * as hljs from "highlight.js";
+import hljs from "highlight.js/lib/core";
+import xml from "highlight.js/lib/languages/xml";
+import java from "highlight.js/lib/languages/java";
+import gradle from "highlight.js/lib/languages/gradle";
+
+hljs.registerLanguage("xml", xml);
+hljs.registerLanguage("java", java);
+hljs.registerLanguage("gradle", gradle);
 
 addEventListener(
     "message",
